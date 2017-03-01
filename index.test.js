8b7a190513df9ab37a2983546b00414b41c61487
Jaroslav Kubicek <jaroslav.kubicek@merck.com>
2017-03-01 10:18:19 +0100

    more examples for practising added

--- index.test.js
+++ index.test.js
@@ -17,4 +17,34 @@ describe('Introductory traps', function () {
       expect(sequence).toEqual([1, 2, 3]);
     });
   });
+
+  describe('getAdults', function () {
+    it('should return people older than 18 years separated by comma', function () {
+      const people = [
+        {firstName: 'John', surname: 'Smith', age: 30},
+        {firstName: 'Peter', surname: 'Pan', age: 12},
+        {firstName: 'Adriane', surname: 'Miller', age: 65}
+      ];
+
+      const adults = traps.getAdults(people);
+      expect(adults).toBe('John Smith, Adriane Miller');
+    });
+  });
+
+  describe('fetchData', function () {
+    it('handle error properly', function (done) {
+      traps.fetchData('nonsense', function (err) {
+        expect(err).toBeA(Error);
+        done();
+      })
+    });
+
+    it('fetches data successfully', function (done) {
+      traps.fetchData(2, function (err, data) {
+        expect(data).toBeA('object');
+        expect(data).toInclude({title: 'king'});
+        done(err);
+      })
+    });
+  });
 });
